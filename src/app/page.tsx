9942237fef59fdec164a90859acf16290992d5e0
RemiLeBeau <no@thanks.com>
2023-12-11 14:24:05 -0500

    moved footer and header to layout.tsx and cut main.tsx into page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,51 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Main from "./components/Main";
+import Link from "next/link";
 
-export default function Home() {
+export default function HomePage() {
+  const projectNames = [
+    "nextjs-blogproject",
+    "nextjs-wikirocket",
+    "python-weather",
+    "chat-app",
+    "typescript-todolist",
+    "typescript-shoppingcart",
+  ];
+  const projectLinks = [
+    "https://nextjs-blogproject.vercel.app/",
+    "https://nextjs-wikirocket-black.vercel.app/",
+    "https://python-weather-o9ui.onrender.com/",
+    "https://io-project-kqna.onrender.com/",
+    "https://typescript-todolist.onrender.com/",
+    "https://typescript-shoppingcart.onrender.com/",
+  ];
+
+  const projects = Object.fromEntries(
+    projectNames.map((projectName, index) => [projectName, projectLinks[index]])
+  );
+
   return (
-    <>
-      <Header />
-
-      <Main />
-      <Footer />
-    </>
+    <main className="p-4 text-center text-3xl font-bold">
+      <section>
+        <h2>Technologies</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Tailwind</li>
+        </ul>
+      </section>
+      <br />
+      <section>
+        <h2>Projects</h2>
+        {/* list of projects with their links */}
+        <ul>
+          {Object.entries(projects).map(([projectName, projectLink]) => (
+            <li key={projectName}>
+              <Link href={projectLink}>{projectName}</Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </main>
   );
 }
